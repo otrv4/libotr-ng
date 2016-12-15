8fe184fa702470a0b71334c1d372c1e19e02d3b1
Iván Pazmiño <iapazmino@gmail.com>
2016-12-15 10:40:31 -0500

    Refactors test suite to use fixture set up and teardown.

--- src/test/test_otr.c
+++ src/test/test_otr.c
@@ -5,6 +5,22 @@
 
 #include "../otr.h"
 
+typedef struct {
+  otr *otr;
+} otr_fixture;
+
+void
+otr_fixture_set_up(otr_fixture *otr_fixture, gconstpointer data) {
+  otr *otr = otr_malloc();
+  otr_start(otr);
+  otr_fixture->otr = otr;
+}
+
+void
+otr_fixture_teardown(otr_fixture *otr_fixture, gconstpointer data) {
+  otr_free(otr_fixture->otr);
+}
+
 void
 test_otr_starts_protocol() {
   otr *otr = otr_malloc();
@@ -19,79 +35,56 @@ test_otr_starts_protocol() {
 }
 
 void
-test_otr_version_support_v34() {
-  otr *otr = otr_malloc();
-  otr_start(otr);
+test_otr_version_support_v34(otr_fixture *otr_fixture, gconstpointer data) {
+  otr_version_support_v3(otr_fixture->otr);
 
-  otr_version_support_v3(otr);
-
-  g_assert_cmpint(otr->supported_versions, ==, OTR_ALLOW_V3 | OTR_ALLOW_V4);
-
-  otr_free(otr);
+  g_assert_cmpint(otr_fixture->otr->supported_versions, ==, OTR_ALLOW_V3 | OTR_ALLOW_V4);
 }
 
 void
-test_otr_builds_query_message() {
-  otr *otr = otr_malloc();
-  otr_start(otr);
+test_otr_builds_query_message(otr_fixture *otr_fixture, gconstpointer data) {
   char *message = "And some random invitation text.";
 
   char query_message[41];
-  otr_build_query_message(query_message, otr, message);
+  otr_build_query_message(query_message, otr_fixture->otr, message);
 
   char *expected_qm = "?OTRv4? And some random invitation text.";
   g_assert_cmpstr(query_message, ==, expected_qm);
-
-  otr_free(otr);
 }
 
 void
-test_otr_builds_query_message_v34() {
-  otr *otr = otr_malloc();
-  otr_start(otr);
-  otr_version_support_v3(otr);
+test_otr_builds_query_message_v34(otr_fixture *otr_fixture, gconstpointer data) {
+  otr_version_support_v3(otr_fixture->otr);
   char *message = "And some random invitation text.";
 
   char query_message[41];
-  otr_build_query_message(query_message, otr, message);
+  otr_build_query_message(query_message, otr_fixture->otr, message);
 
   char *expected_qm = "?OTRv34? And some random invitation text.";
   g_assert_cmpstr(query_message, ==, expected_qm);
-
-  otr_free(otr);
 }
 
 void
-test_otr_builds_whitespace_tag() {
-  otr *otr = otr_malloc();
-  otr_start(otr);
-
+test_otr_builds_whitespace_tag(otr_fixture *otr_fixture, gconstpointer data) {
   char *expected_tag = " \t  \t\t\t\t \t \t \t    \t\t \t  And some random invitation text.";
   char *message = "And some random invitation text.";
 
   char whitespace_tag[strlen(expected_tag)];
-  int error = otr_build_whitespace_tag(whitespace_tag, otr, message);
+  int error = otr_build_whitespace_tag(whitespace_tag, otr_fixture->otr, message);
   g_assert_false(error);
   g_assert_cmpstr(whitespace_tag, ==, expected_tag);
-
-  otr_free(otr);
 }
 
 void
-test_otr_builds_whitespace_tag_v34() {
-  otr *otr = otr_malloc();
-  otr_start(otr);
-  otr_version_support_v3(otr);
-
+test_otr_builds_whitespace_tag_v34(otr_fixture *otr_fixture, gconstpointer data) {
+  otr_version_support_v3(otr_fixture->otr);
   char *expected_tag = " \t  \t\t\t\t \t \t \t    \t\t \t    \t\t  \t\tAnd some random invitation text";
   char *message = "And some random invitation text";
 
   char whitespace_tag[strlen(expected_tag)];
-  int error = otr_build_whitespace_tag(whitespace_tag, otr, message);
+  int error = otr_build_whitespace_tag(whitespace_tag, otr_fixture->otr, message);
   g_assert_false(error);
   g_assert_cmpstr(whitespace_tag, ==, expected_tag);
-
-  otr_free(otr);
 }
 
 int
@@ -99,11 +92,11 @@ main(int argc, char **argv) {
   g_test_init(&argc, &argv, NULL);
 
   g_test_add_func("/otr_starts_protocol", test_otr_starts_protocol);
-  g_test_add_func("/otr_version_support_v34", test_otr_version_support_v34);
-  g_test_add_func("/otr_builds_query_message", test_otr_builds_query_message);
-  g_test_add_func("/otr_builds_query_message_34", test_otr_builds_query_message_v34);
-  g_test_add_func("/otr_builds_whitespace_tag", test_otr_builds_whitespace_tag);
-  g_test_add_func("/otr_builds_whitespace_tag_v34", test_otr_builds_whitespace_tag_v34);
+  g_test_add("/otr_version_support_v34", otr_fixture, NULL, otr_fixture_set_up, otr_fixture_teardown, test_otr_version_support_v34);
+  g_test_add("/otr_builds_query_message", otr_fixture, NULL, otr_fixture_set_up, otr_fixture_teardown, test_otr_builds_query_message);
+  g_test_add("/otr_builds_query_message_34", otr_fixture, NULL, otr_fixture_set_up, otr_fixture_teardown, test_otr_builds_query_message_v34);
+  g_test_add("/otr_builds_whitespace_tag", otr_fixture, NULL, otr_fixture_set_up, otr_fixture_teardown, test_otr_builds_whitespace_tag);
+  g_test_add("/otr_builds_whitespace_tag_v34", otr_fixture, NULL, otr_fixture_set_up, otr_fixture_teardown, test_otr_builds_whitespace_tag_v34);
 
   return g_test_run();
 }
