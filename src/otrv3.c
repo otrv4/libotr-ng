851456ad5a3ff0ccc31f416862f1d8c37ccba9da
Iván Pazmiño <iapazmino@gmail.com>
2017-01-05 13:56:30 -0500

    Notes on usage of libotr for v3 messages.

--- src/otrv3.c
+++ src/otrv3.c
@@ -1,3 +1,12 @@
+/**
+ * TODO: This is where we are going to delegate execution of
+ * OTR v3 messages. Fot this to work we (think) need to:
+ * 1. include libotr's libraries. See usage here 
+ * https://bugs.otr.im/lib/libotr/blob/master/README
+ * 2. (maybe) rename the otr_t structure to otrv4_t to avoid name conflicts
+ * with the libotr library.
+ * 3. Include libotr in the Makefile as a dependency
+ */
 #include "otrv3.h"
 
 void
