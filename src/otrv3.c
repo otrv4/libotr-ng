e17551b9061aae51ce52daf6cc06bb5a1ee342f2
Iván Pazmiño <iapazmino@gmail.com>
2017-01-05 12:29:44 -0500

    Fixes receiving plaintext with tag v3.

--- src/otrv3.c
+++ src/otrv3.c
@@ -1,10 +1,6 @@
-#include <string.h>
-
 #include "otrv3.h"
-#include "mem.h"
 
 void
 otrv3_receive_message(char **response, const char *message) {
-  *response = mem_alloc(strlen(message));
-  strcpy(*response, message);
+  *response = strdup(message);
 }
