32df9751d55cf170814da5c0985f7914480bb756
Reinaldo de Souza Jr <juniorz@gmail.com>
2017-01-26 14:49:27 -0500

    Use autoreconf

--- autogen.sh
+++ autogen.sh
@@ -2,20 +2,5 @@
 
 set -e
 
-# TODO "autoreconf --install" seems to be enough. But it requires autotools >= 1.6
-
-case "$(uname)" in
-    Darwin)
-        LIBTOOLIZE=${LIBTOOLIZE:-glibtoolize}
-        ;;
-    *)
-        LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
-        ;;
-esac
-
-aclocal -I m4 && \
-  ${LIBTOOLIZE} --automake --copy && \
-  autoconf && \
-  autoheader && \
-  automake --add-missing --copy
+autoreconf --install --force
 
