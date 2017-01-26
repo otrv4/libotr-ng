6900b9575fcc704cb88904c2d43b484f77f47a7c
Fan Jiang <fan.torchz@gmail.com>
2017-01-26 15:43:26 -0500

    switch to libgcrypt

--- src/random.h
+++ src/random.h
@@ -1,12 +1,11 @@
-#include <sodium.h>
+#include <gcrypt.h>
 
 #ifndef RANDOM_H
 #define RANDOM_H
 
-//TODO this could be from gcrypt since we already use it for the DH
 static void inline
 random_bytes(void * const buf, const size_t size) {
-  randombytes_buf(buf, size);
+  gcry_randomize(buf, size, GCRY_STRONG_RANDOM);
 }
 
 #endif
