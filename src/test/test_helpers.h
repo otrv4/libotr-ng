b2d83f19310f3a8bc44ade1326a2610171df5665
Reinaldo de Souza Jr <juniorz@gmail.com>
2017-01-26 22:16:45 -0500

    Add more assertions

--- src/test/test_helpers.h
+++ src/test/test_helpers.h
@@ -36,6 +36,24 @@ otrv4_memdump(const uint8_t *src, size_t len) {
                         g_assertion_message_expr (G_LOG_DOMAIN, __FILE__, __LINE__, G_STRFUNC, \
                         #expr); } while (0)
 
+#define otrv4_assert_cs_public_key_equals(pk1, pk2) do { \
+  otrv4_assert_point_equals(pk1->c, pk2->c); \
+  otrv4_assert_point_equals(pk1->d, pk2->d); \
+  otrv4_assert_point_equals(pk1->h, pk2->h); \
+} while (0)
+
+#define otrv4_assert_mpi_eq(m1, m2) do { \
+  g_assert_cmpuint(m1->len, ==, m2->len); \
+  otrv4_assert_cmpmem(m1->data, m2->data, m1->len); \
+} while (0)
+
+#define otrv4_assert_user_profile_eq(p1, p2) do { \
+  otrv4_assert_cs_public_key_equals(p1->pub_key, p2->pub_key); \
+  otrv4_assert_cmpmem(p1->versions, p2->versions, strlen(p1->versions)+1); \
+  g_assert_cmpuint(p1->expires, ==, p2->expires); \
+  otrv4_assert_cmpmem(p1->signature, p2->signature, EC_SIGNATURE_BYTES); \
+  otrv4_assert_mpi_eq(p1->transitional_signature, p2->transitional_signature); \
+} while (0)
 
 static inline void
 otrv4_assert_point_equals(const ec_point_t expected, const ec_point_t actual) {
