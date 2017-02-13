6006eea6e3885491310869a1f2be86da2aad1e19
Reinaldo de Souza Jr <juniorz@gmail.com>
2017-02-13 17:16:17 -0500

    Revert "Revert "add test for derive_ratchet_keys""
    This reverts commit b8e6f26d790ba9089fa84d7519abd78f0bbf2553.

--- src/key_management.c
+++ src/key_management.c
@@ -6,38 +6,29 @@
 #include "key_management.h"
 
 void
-append_ratchet(key_manager_t manager, ratchet_t r)
-{
+append_ratchet(key_manager_t manager, ratchet_t r){
     manager->current->next = r;
     manager->current = r;
 }
 
 void
 init_key_manager(key_manager_t manager){
-    ratchet_t head;
-    manager->head = head;
-    manager->current = head;
+    ratchet_t *head = malloc( sizeof(ratchet_t) );
+    manager->head = *head;
+    manager->current = *head;
 }
 
 void
-derive_ratchet_keys(key_manager_t manager, const uint8_t *shared)
+derive_ratchet_keys(key_manager_t manager, const uint8_t *shared, size_t size)
 {
-    gcry_md_hd_t sha3_512;
-    ratchet_t next_ratchet;
+    gcry_md_hd_t hd;
     uint8_t magic[3] = {0x00, 0x01, 0x02};
-    gcry_md_open(&sha3_512, GCRY_MD_SHA3_512, GCRY_MD_FLAG_SECURE);
-    gcry_md_write(sha3_512, shared, sizeof(shared));
-    gcry_md_write(sha3_512, &magic[0], 1);
-    gcry_md_extract(sha3_512, GCRY_MD_SHA3_512, next_ratchet->root_key, sizeof(root_key_t));
+    ratchet_t *next_ratchet = malloc( sizeof(ratchet_t) );
+    gcry_md_open(&hd, GCRY_MD_SHA3_512, GCRY_MD_FLAG_SECURE);
+    gcry_md_write(hd, shared, size);
+    gcry_md_write(hd, &magic[0], 1);
+    memcpy(next_ratchet[0]->root_key, gcry_md_read(hd, 0), sizeof(root_key_t));
 
-    gcry_md_reset(sha3_512);
-    gcry_md_write(sha3_512, shared, sizeof(shared));
-    gcry_md_write(sha3_512, &magic[1], 1);
-    gcry_md_extract(sha3_512, GCRY_MD_SHA3_512, next_ratchet->chain_keys_a[0], sizeof(root_key_t));
-
-    gcry_md_reset(sha3_512);
-    gcry_md_write(sha3_512, shared, sizeof(shared));
-    gcry_md_write(sha3_512, &magic[2], 1);
-    gcry_md_extract(sha3_512, GCRY_MD_SHA3_512, next_ratchet->chain_keys_b[0], sizeof(root_key_t));
-    append_ratchet(manager, next_ratchet);
+    gcry_md_close(hd);
+    append_ratchet(manager, *next_ratchet);
 }
--- src/test/test_key_management.c
+++ src/test/test_key_management.c
@@ -3,7 +3,30 @@
 void
 test_derive_ratchet_keys() {
     key_manager_t km;
-    uint8_t shared[56] = "hello";
     init_key_manager(km);
-    derive_ratchet_keys(km, shared);
+    uint8_t shared[56] = {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
+    derive_ratchet_keys(km, shared, sizeof(shared));
+    derive_ratchet_keys(km, shared, sizeof(shared));
+
+    gcry_md_hd_t sha3_512;
+    ratchet_t next_ratchet;
+    uint8_t magic[3] = {0x00, 0x01, 0x02};
+    gcry_md_open(&sha3_512, GCRY_MD_SHA3_512, GCRY_MD_FLAG_SECURE);
+    gcry_md_write(sha3_512, shared, 56);
+    gcry_md_write(sha3_512, &magic[0], 1);
+    memcpy(next_ratchet->root_key, gcry_md_read(sha3_512, 0), sizeof(root_key_t));
+    gcry_md_close(sha3_512);
+
+    otrv4_assert_cmpmem(next_ratchet->root_key, km->current->root_key, sizeof(root_key_t));
+    otrv4_assert_cmpmem(next_ratchet->root_key, km->head->next->next->root_key, sizeof(root_key_t));
+    otrv4_assert(km->current == km->head->next->next);
+    otrv4_assert(km->current != km->head->next);
 }
