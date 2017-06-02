d07de89d899965672648238e031e3403822a1357
Sofia Celi <sceli@thoughtworks.com>
2017-06-02 10:28:26 -0500

    reverts free

--- src/otrv4.c
+++ src/otrv4.c
@@ -1393,7 +1393,7 @@ otr4_err_t otrv4_smp_start(string_t *to_send, const string_t question,
   case OTRV4_VERSION_4:
     smp_start_tlv =
         otrv4_smp_initiate(otr, question, secret, secretlen);
-	free(smp_start_tlv);
+    // free tlv
     return otrv4_send_message(to_send, "", smp_start_tlv, otr);
     break;
   case OTRV4_VERSION_NONE:
