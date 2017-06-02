11a330f9b8266cb8144c7372d5248103b452dcd1
Sofia Celi <sceli@thoughtworks.com>
2017-06-02 18:12:49 -0500

    frees tlv 3

--- src/otrv4.c
+++ src/otrv4.c
@@ -1394,20 +1394,17 @@ otr4_err_t otrv4_smp_start(string_t *to_send, const string_t question,
     smp_start_tlv =
         otrv4_smp_initiate(otr, question, secret, secretlen);
     if (otrv4_send_message(to_send, "", smp_start_tlv, otr)) {
-      free(smp_start_tlv);
-      smp_start_tlv = NULL;
+      otrv4_tlv_free(smp_start_tlv);
       return OTR4_ERROR;
     }
-    free(smp_start_tlv);
-    smp_start_tlv = NULL;
+    otrv4_tlv_free(smp_start_tlv);
     return OTR4_SUCCESS;
     break;
   case OTRV4_VERSION_NONE:
     return OTR4_ERROR;
   }
 
-  free(smp_start_tlv);
-  smp_start_tlv = NULL;
+  otrv4_tlv_free(smp_start_tlv);
   return OTR4_ERROR;
 }
 
