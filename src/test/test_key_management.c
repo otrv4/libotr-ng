af208c75d29c85ea1f1b55fbc50d46f44dfd7393
Natalie Eskinazi <n.l.eskinazi@gmail.com>
2017-08-02 17:30:41 -0500

    Add test for derive_chain_key_b

--- src/test/test_key_management.c
+++ src/test/test_key_management.c
@@ -21,20 +21,23 @@ void test_derive_ratchet_keys() {
 
   root_key_t expected_root_key;
   chain_key_t expected_chain_key_a;
+  chain_key_t expected_chain_key_b;
 
   gcry_md_hd_t sha3_512;
   uint8_t magic[3] = {0x01, 0x02, 0x03};
 
   create_sha3_512_buffer(&sha3_512, &magic[0]);
   memcpy(expected_root_key, gcry_md_read(sha3_512, 0), sizeof(root_key_t));
-
-  otrv4_assert_cmpmem(expected_root_key, manager.current->root_key, sizeof(root_key_t));
-
   create_sha3_512_buffer(&sha3_512, &magic[1]);
   memcpy(expected_chain_key_a, gcry_md_read(sha3_512, 0), sizeof(chain_key_t));
+  create_sha3_512_buffer(&sha3_512, &magic[2]);
+  memcpy(expected_chain_key_b, gcry_md_read(sha3_512, 0), sizeof(chain_key_t));
+
   gcry_md_close(sha3_512);
 
+  otrv4_assert_cmpmem(expected_root_key, manager.current->root_key, sizeof(root_key_t));
   otrv4_assert_cmpmem(expected_chain_key_a, manager.current->chain_a->key, sizeof(chain_key_t));
+  otrv4_assert_cmpmem(expected_chain_key_b, manager.current->chain_b->key, sizeof(chain_key_t));
 }
 
 void test_key_manager_destroy() {
