4ffecc2a4c6c093b26a9941f4b115b6839ec05f6
Ola Bini <ola@olabini.se>
2018-04-05 16:44:57 -0500

    Fix #51 - Include licensing and copyright information

--- src/debug.h
+++ src/debug.h
@@ -1,3 +1,23 @@
+/*
+ *  This file is part of the Off-the-Record Next Generation Messaging
+ *  library (libotr-ng).
+ *
+ *  Copyright (C) 2016-2018, the libotr-ng contributors.
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // This file doesn't have the standard header file guard - the reason for that
 // has to do with weird interactions with static inline and so on.
 // It's a bit horrible, but let's keep it this way for now.
