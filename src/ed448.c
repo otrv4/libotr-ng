9eeeff840e4388293ea69c73614de5a434c04a06
Sofía Celi <cherenkovd69@gmail.com>
2018-04-11 12:47:50 -0500

    Start documenting functions for points and scalars #57
    - Expose point_copy function that was not on header file.
    - Change variable names

--- src/ed448.c
+++ src/ed448.c
@@ -29,6 +29,10 @@ INTERNAL void otrng_ec_bzero(void *data, size_t size) {
   goldilocks_bzero(data, size);
 }
 
+INTERNAL void otrng_ec_scalar_copy(ec_scalar_t dst, const ec_scalar_t a) {
+  goldilocks_448_scalar_copy(dst, a);
+}
+
 INTERNAL otrng_bool_t otrng_ec_scalar_eq(const ec_scalar_t a,
                                          const ec_scalar_t b) {
   if (goldilocks_448_scalar_eq(a, b))
@@ -37,12 +41,76 @@ INTERNAL otrng_bool_t otrng_ec_scalar_eq(const ec_scalar_t a,
   return otrng_false;
 }
 
+INTERNAL void otrng_ec_scalar_encode(uint8_t *enc, const ec_scalar_t s) {
+  goldilocks_448_scalar_encode(enc, s);
+}
+
+INTERNAL void otrng_ec_scalar_decode(ec_scalar_t s,
+                                     const uint8_t enc[ED448_SCALAR_BYTES]) {
+  goldilocks_448_scalar_decode_long(s, enc, ED448_SCALAR_BYTES);
+}
+
+INTERNAL void otrng_ec_scalar_destroy(ec_scalar_t s) {
+  goldilocks_448_scalar_destroy(s);
+}
+
 INTERNAL void otrng_ec_point_copy(ec_point_t dst, const ec_point_t src) {
   goldilocks_448_point_copy(dst, src);
 }
 
-INTERNAL void otrng_ec_point_destroy(ec_point_t dst) {
-  goldilocks_448_point_destroy(dst);
+INTERNAL otrng_bool_t otrng_ec_point_eq(const ec_point_t p,
+                                        const ec_point_t q) {
+  if (goldilocks_448_point_eq(p, q))
+    return otrng_true;
+
+  return otrng_false;
+}
+
+INTERNAL otrng_bool_t otrng_ec_point_valid(const ec_point_t p) {
+  if (goldilocks_448_point_valid(p))
+    return otrng_true;
+
+  return otrng_false;
+}
+
+INTERNAL void otrng_ec_point_encode(uint8_t *enc, const ec_point_t p) {
+  goldilocks_448_point_mul_by_ratio_and_encode_like_eddsa(enc, p);
+}
+
+INTERNAL otrng_err_t
+otrng_ec_point_decode(ec_point_t p, const uint8_t enc[ED448_POINT_BYTES]) {
+  goldilocks_448_point_t tmp_p;
+  goldilocks_error_t err =
+      goldilocks_448_point_decode_like_eddsa_and_mul_by_ratio(tmp_p, enc);
+  if (GOLDILOCKS_SUCCESS != err)
+    return ERROR;
+
+  // The decoded point is equal to the original point * 2^2
+  goldilocks_448_scalar_t r;
+  goldilocks_448_scalar_copy(r, goldilocks_448_scalar_one);
+  goldilocks_448_scalar_halve(r, r);
+  goldilocks_448_scalar_halve(r, r);
+
+  goldilocks_448_point_scalarmul(p, tmp_p, r);
+
+  return SUCCESS;
+}
+
+INTERNAL void otrng_ec_point_destroy(ec_point_t p) {
+  goldilocks_448_point_destroy(p);
+}
+
+INTERNAL void
+otrng_ec_scalar_derive_from_secret(ec_scalar_t priv,
+                                   uint8_t sym[ED448_PRIVATE_BYTES]) {
+  /* Hash and clamp the secret into a scalar */
+  goldilocks_ed448_derive_secret_scalar(priv, sym);
+}
+
+INTERNAL void
+otrng_ec_derive_public_key(uint8_t pub[ED448_POINT_BYTES],
+                           const uint8_t sym[ED448_PRIVATE_BYTES]) {
+  goldilocks_ed448_derive_public_key(pub, sym);
 }
 
 INTERNAL void otrng_ecdh_keypair_generate(ecdh_keypair_t *keypair,
@@ -62,19 +130,6 @@ INTERNAL void otrng_ecdh_keypair_destroy(ecdh_keypair_t *keypair) {
   otrng_ec_point_destroy(keypair->pub);
 }
 
-INTERNAL void
-otrng_ec_scalar_derive_from_secret(ec_scalar_t priv,
-                                   uint8_t sym[ED448_PRIVATE_BYTES]) {
-  /* Hash and clamp the secret into a scalar */
-  goldilocks_ed448_derive_secret_scalar(priv, sym);
-}
-
-INTERNAL void
-otrng_ec_derive_public_key(uint8_t pub[ED448_POINT_BYTES],
-                           const uint8_t sym[ED448_PRIVATE_BYTES]) {
-  goldilocks_ed448_derive_public_key(pub, sym);
-}
-
 INTERNAL void otrng_ecdh_shared_secret(uint8_t *shared,
                                        const ecdh_keypair_t *our_keypair,
                                        const ec_point_t their_pub) {
@@ -87,39 +142,6 @@ INTERNAL void otrng_ecdh_shared_secret(uint8_t *shared,
 /* void ec_public_key_copy(ec_public_key_t dst, const ec_public_key_t src) { */
 /*   memcpy(dst, src, sizeof(ec_public_key_t)); */
 /* } */
-
-INTERNAL void otrng_ec_scalar_encode(uint8_t *dst, const ec_scalar_t scalar) {
-  goldilocks_448_scalar_encode(dst, scalar);
-}
-
-INTERNAL void
-otrng_ec_scalar_decode(ec_scalar_t scalar,
-                       const uint8_t serialized[ED448_SCALAR_BYTES]) {
-  goldilocks_448_scalar_decode_long(scalar, serialized, ED448_SCALAR_BYTES);
-}
-
-INTERNAL void otrng_ec_point_encode(uint8_t *dst, const ec_point_t point) {
-  goldilocks_448_point_mul_by_ratio_and_encode_like_eddsa(dst, point);
-}
-
-INTERNAL otrng_err_t otrng_ec_point_decode(
-    ec_point_t point, const uint8_t serialized[ED448_POINT_BYTES]) {
-  goldilocks_448_point_t p;
-  goldilocks_error_t err =
-      goldilocks_448_point_decode_like_eddsa_and_mul_by_ratio(p, serialized);
-  if (GOLDILOCKS_SUCCESS != err)
-    return ERROR;
-
-  // The decoded point is equal to the original point * 2^2
-  goldilocks_448_scalar_t r;
-  goldilocks_448_scalar_copy(r, goldilocks_448_scalar_one);
-  goldilocks_448_scalar_halve(r, r);
-  goldilocks_448_scalar_halve(r, r);
-
-  goldilocks_448_point_scalarmul(point, p, r);
-
-  return SUCCESS;
-}
 
 static const char *ctx = "";
 
@@ -136,29 +158,6 @@ INTERNAL otrng_bool_t otrng_ec_verify(const uint8_t sig[ED448_SIGNATURE_BYTES],
                                       const uint8_t *msg, size_t msg_len) {
   if (goldilocks_ed448_verify(sig, pubkey, msg, msg_len, 0, (uint8_t *)ctx,
                               strlen(ctx)))
-    return otrng_true;
-
-  return otrng_false;
-}
-
-INTERNAL void otrng_ec_scalar_copy(ec_scalar_t dst, const ec_scalar_t src) {
-  goldilocks_448_scalar_copy(dst, src);
-}
-
-INTERNAL void otrng_ec_scalar_destroy(ec_scalar_t dst) {
-  goldilocks_448_scalar_destroy(dst);
-}
-
-INTERNAL otrng_bool_t otrng_ec_point_valid(const ec_point_t point) {
-  if (goldilocks_448_point_valid(point))
-    return otrng_true;
-
-  return otrng_false;
-}
-
-INTERNAL otrng_bool_t otrng_ec_point_eq(const ec_point_t p,
-                                        const ec_point_t q) {
-  if (goldilocks_448_point_eq(p, q))
     return otrng_true;
 
   return otrng_false;
--- src/ed448.h
+++ src/ed448.h
@@ -28,14 +28,21 @@
 #include "error.h"
 #include "shared.h"
 
-/* goldilocks_448_point_t is in the twisted ed448-goldilocks. */
+/* goldilocks_448_point_t is in the twisted ed448-goldilocks,
+   following the decaf technique. */
 typedef goldilocks_448_scalar_t ec_scalar_t;
 typedef goldilocks_448_point_t ec_point_t;
 
-/* Serialize points and scalars using EdDSA wire format. */
+/** Number of bytes in an EdDSA private key. */
 #define ED448_PRIVATE_BYTES GOLDILOCKS_EDDSA_448_PRIVATE_BYTES
+
+/** Number of bytes in an EdDSA public key. */
 #define ED448_POINT_BYTES GOLDILOCKS_EDDSA_448_PUBLIC_BYTES
+
+/** Number of bytes in an EdDSA signature. */
 #define ED448_SIGNATURE_BYTES GOLDILOCKS_EDDSA_448_SIGNATURE_BYTES
+
+/** Number of bytes in an non-secret scalar. */
 #define ED448_SCALAR_BYTES GOLDILOCKS_448_SCALAR_BYTES
 
 typedef uint8_t goldilocks_448_public_key_t[ED448_POINT_BYTES];
@@ -49,35 +56,84 @@ typedef struct {
 
 typedef goldilocks_448_public_key_t ec_public_key_t;
 
+/**
+ * @brief Overwrite data with zeros.  Uses memset_s if available.
+ *
+ * @param data The data to be zeroed.
+ * @param size The size of the data.
+ */
 INTERNAL void otrng_ec_bzero(void *data, size_t size);
 
+/**
+ * @brief Copy a scalar.  The scalars may use the same memory, in which
+ * case this function does nothing.
+ * @param [in] a A scalar.
+ * @param [out] out Will become a copy of a.
+ */
+INTERNAL void otrng_ec_scalar_copy(ec_scalar_t dst, const ec_scalar_t a);
+
+/**
+ * @brief Compare two scalars.
+ * @param [in] a One scalar.
+ * @param [in] b Another scalar.
+ * @retval otrng_true The scalars are equal.
+ * @retval otrng_false The scalars are not equal.
+ */
 INTERNAL otrng_bool_t otrng_ec_scalar_eq(const ec_scalar_t a,
                                          const ec_scalar_t b);
+/**
+ * @brief Encode a scalar to wire format.
+ *
+ * @param [out] enc Encoded form of a scalar.
+ * @param [in] s Deserialized scalar.
+ */
+INTERNAL void otrng_ec_scalar_encode(uint8_t *enc, const ec_scalar_t s);
 
-INTERNAL void otrng_ec_scalar_encode(uint8_t *dst, const ec_scalar_t scalar);
+/**
+ * @brief Read a scalar from wire format or from bytes.  Reduces mod
+ * scalar prime.
+ *
+ * @param [in] enc Encoded form of a scalar.
+ * @param [out] s Deserialized form.
+ */
+INTERNAL void otrng_ec_scalar_decode(ec_scalar_t s,
+                                     const uint8_t enc[ED448_SCALAR_BYTES]);
 
-INTERNAL void
-otrng_ec_scalar_decode(ec_scalar_t scalar,
-                       const uint8_t serialized[ED448_SCALAR_BYTES]);
-
-INTERNAL void otrng_ec_scalar_copy(ec_scalar_t dst, const ec_scalar_t src);
-
-INTERNAL void otrng_ec_scalar_destroy(ec_scalar_t dst);
-
-INTERNAL void otrng_ec_point_copy(ec_point_t dst, const ec_point_t src);
+/** Securely erase a scalar. */
+INTERNAL void otrng_ec_scalar_destroy(ec_scalar_t s);
 
 INTERNAL void otrng_ec_point_destroy(ec_point_t dst);
 
-INTERNAL otrng_bool_t otrng_ec_point_valid(const ec_point_t point);
+INTERNAL otrng_bool_t otrng_ec_point_valid(const ec_point_t p);
 
 INTERNAL otrng_bool_t otrng_ec_point_eq(const ec_point_t, const ec_point_t);
 
-INTERNAL void otrng_ec_point_encode(uint8_t *dst, const ec_point_t point);
+/**
+ * @brief EdDSA point encoding.
+ * Multiplies by the cofactor first.
+ *
+ * The multiplication is required because the EdDSA encoding represents
+ * the cofactor information, but the Decaf encoding ignores it (which
+ * is the whole point).  So if you decode from EdDSA and re-encode to
+ * EdDSA, the cofactor info must get cleared, because the intermediate
+ * representation doesn't track it.
+ *
+ * @param [out] enc The encoded point.
+ * @param [in] p The point.
+ */
+INTERNAL void otrng_ec_point_encode(uint8_t *enc, const ec_point_t p);
 
-INTERNAL otrng_err_t otrng_ec_point_decode(
-    ec_point_t point, const uint8_t serialized[ED448_POINT_BYTES]);
+/**
+ * @brief EdDSA point decoding.
+ *
+ * @param [out] enc The encoded point.
+ * @param [in] p The point.
+ */
+INTERNAL otrng_err_t
+otrng_ec_point_decode(ec_point_t p, const uint8_t enc[ED448_POINT_BYTES]);
 
-/* This is ed448 crypto */
+INTERNAL void otrng_ec_point_copy(ec_point_t dst, const ec_point_t src);
+
 INTERNAL void
 otrng_ec_scalar_derive_from_secret(ec_scalar_t priv,
                                    uint8_t sym[ED448_PRIVATE_BYTES]);
