a9956b99077219f321755a2eccbb17594fc3ab92
Giovane Liberato <giovane@autonomia.digital>
2018-04-17 17:40:05 -0300

    Add KDF1 usage id to the mixed shared secret calculation from tmp key #60

--- src/key_management.c
+++ src/key_management.c
@@ -401,11 +401,13 @@ tstatic void calculate_shared_secret(shared_secret_p dst, const k_ecdh_p k_ecdh,
 }
 
 tstatic void calculate_shared_secret_from_tmp_key(shared_secret_p dst,
-                                                  const uint8_t *tmp_k) {
+                                                  const uint8_t *tmp_k,
+                                                  const brace_key_p brace_key) {
   goldilocks_shake256_ctx_p hd;
 
-  hash_init_with_dom(hd);
-  hash_update(hd, tmp_k, HASH_BYTES);
+  hash_init_with_usage(hd, 0x04);
+  hash_update(hd, tmp_k, sizeof(uint8_t));
+  hash_update(hd, brace_key, sizeof(brace_key_p));
 
   hash_final(hd, dst, sizeof(shared_secret_p));
   hash_destroy(hd);
@@ -517,7 +519,7 @@ tstatic otrng_err init_ratchet(key_manager_s *manager, bool interactive) {
   if (interactive)
     calculate_shared_secret(shared_secret, k_ecdh, manager->brace_key);
   else
-    calculate_shared_secret_from_tmp_key(shared_secret, manager->tmp_key);
+    calculate_shared_secret_from_tmp_key(shared_secret, manager->tmp_key, manager->brace_key);
 
 #ifdef DEBUG
   printf("ENTERING NEW RATCHET\n");
