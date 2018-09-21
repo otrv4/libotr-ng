c7ff736253e04a454d22143ea172c724f7e21315
Ola Bini <ola@olabini.se>
2018-09-21 19:14:26 -0300

    Fix an unused memory problem causing the build to break

--- src/test/test_messaging.c
+++ src/test/test_messaging.c
@@ -28,10 +28,12 @@ static const char *charlie_account = "charlie@xmpp";
 
 static const otrng_client_id_s read_client_id_for_privf(FILE *privf) {
   char *line = malloc(50 * sizeof(char));
-  int len;
-  fgets(line, 50, privf);
-  len = strlen(line);
-  free(line);
+  int len = 0;
+  char *line2 = fgets(line, 50, privf);
+  if (line2 != NULL) {
+    len = strlen(line2);
+    free(line);
+  }
 
   otrng_client_id_s result = {
       .protocol = NULL,
