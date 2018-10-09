576d542c4b39e2b1cb0a34d21d82cd0e5dda6a65
Ola Bini <ola@olabini.se>
2018-10-09 09:21:42 +0200

    Update orchestration tests to use fixtures

--- src/test/units/test_orchestration.c
+++ src/test/units/test_orchestration.c
@@ -100,61 +100,120 @@ static void create_prekey_profile(otrng_client_s *client,
   (void)cid;
 }
 
-static void
-test__otrng_client_ensure_correct_state__creates_new_long_term_key(void) {
-  otrng_client_callbacks_s callbacks;
+typedef struct orchestration_fixture_s {
+  otrng_client_callbacks_s *callbacks;
+  otrng_keypair_s *long_term_key;
+  otrng_keypair_s *forging_key;
+  otrng_global_state_s *gs;
+  otrng_client_id_s client_id;
+  otrng_client_s *client;
+  otrng_client_profile_s *client_profile;
+  otrng_prekey_profile_s *prekey_profile;
+} orchestration_fixture_s;
+
+static void orchestration_fixture_setup(orchestration_fixture_s *f,
+                                        gconstpointer user_data) {
   uint8_t sym1[ED448_PRIVATE_BYTES] = {1};
   uint8_t sym2[ED448_PRIVATE_BYTES] = {2};
-  otrng_keypair_s forging_key;
-  otrng_global_state_s gs = {
-      .clients = NULL,
-      .callbacks = &callbacks,
-      .user_state_v3 = NULL,
-  };
-  otrng_client_id_s cid = {
-      .protocol = "test-otr",
-      .account = "sita@otr.im",
-  };
-  otrng_client_s *client = otrng_client_new(cid);
-  client->global_state = &gs;
-  gs.clients = otrng_list_add(client, gs.clients);
-  callbacks.load_privkey_v4 = load_privkey_v4;
-  callbacks.store_privkey_v4 = store_privkey_v4;
-  callbacks.create_privkey_v4 = create_privkey_v4;
-  callbacks.load_client_profile = load_client_profile;
-  callbacks.load_prekey_profile = load_prekey_profile;
-  callbacks.store_client_profile = store_client_profile;
-  callbacks.store_prekey_profile = store_prekey_profile;
-  callbacks.load_prekey_messages = load_prekey_messages;
-  callbacks.store_prekey_messages = store_prekey_messages;
-  callbacks.load_forging_key = load_forging_key;
-  callbacks.store_forging_key = store_forging_key;
-  callbacks.create_forging_key = create_forging_key;
-  callbacks.create_client_profile = create_client_profile;
-  callbacks.store_expired_client_profile = store_expired_client_profile;
-  callbacks.load_expired_client_profile = load_expired_client_profile;
-  callbacks.store_expired_prekey_profile = store_expired_prekey_profile;
-  callbacks.load_expired_prekey_profile = load_expired_prekey_profile;
-  callbacks.create_prekey_profile = create_prekey_profile;
 
-  temp_client = client;
+  (void)user_data;
+
+  f->callbacks = otrng_xmalloc_z(sizeof(otrng_client_callbacks_s));
+  f->gs = otrng_xmalloc_z(sizeof(otrng_global_state_s));
+  f->gs->callbacks = f->callbacks;
+  f->client_id.protocol = "test-otr";
+  f->client_id.account = "sita@otr.im";
+
+  f->client = otrng_client_new(f->client_id);
+  f->client->global_state = f->gs;
+  f->gs->clients = otrng_list_add(f->client, f->gs->clients);
+
+  f->callbacks->load_privkey_v4 = load_privkey_v4;
+  f->callbacks->store_privkey_v4 = store_privkey_v4;
+  f->callbacks->create_privkey_v4 = create_privkey_v4;
+  f->callbacks->load_client_profile = load_client_profile;
+  f->callbacks->load_prekey_profile = load_prekey_profile;
+  f->callbacks->store_client_profile = store_client_profile;
+  f->callbacks->store_prekey_profile = store_prekey_profile;
+  f->callbacks->load_prekey_messages = load_prekey_messages;
+  f->callbacks->store_prekey_messages = store_prekey_messages;
+  f->callbacks->load_forging_key = load_forging_key;
+  f->callbacks->store_forging_key = store_forging_key;
+  f->callbacks->create_forging_key = create_forging_key;
+  f->callbacks->create_client_profile = create_client_profile;
+  f->callbacks->store_expired_client_profile = store_expired_client_profile;
+  f->callbacks->load_expired_client_profile = load_expired_client_profile;
+  f->callbacks->store_expired_prekey_profile = store_expired_prekey_profile;
+  f->callbacks->load_expired_prekey_profile = load_expired_prekey_profile;
+  f->callbacks->create_prekey_profile = create_prekey_profile;
+
+  f->long_term_key = otrng_keypair_new();
+  otrng_keypair_generate(f->long_term_key, sym1);
+
+  f->forging_key = otrng_keypair_new();
+  otrng_keypair_generate(f->forging_key, sym2);
+
+  f->client_profile = otrng_client_profile_build(
+      1234, "4", f->long_term_key, f->forging_key->pub, time(NULL) + 420);
+  f->prekey_profile = otrng_prekey_profile_build(1234, f->long_term_key);
+
+  temp_client = f->client;
 
   load_privkey_v4__called = 0;
   store_privkey_v4__called = 0;
   create_privkey_v4__called = 0;
 
   load_privkey_v4__assign = NULL;
+  create_privkey_v4__assign = NULL;
 
-  create_privkey_v4__assign = otrng_keypair_new();
-  otrng_keypair_generate(create_privkey_v4__assign, sym1);
-  otrng_keypair_generate(&forging_key, sym2);
+  store_privkey_v4__called_with = NULL;
+  load_privkey_v4__called_with.protocol = NULL;
+  load_privkey_v4__called_with.account = NULL;
+  create_privkey_v4__called_with.protocol = NULL;
+  create_privkey_v4__called_with.account = NULL;
+}
 
-  client->client_profile = otrng_client_profile_build(
-      1234, "4", create_privkey_v4__assign, forging_key.pub, time(NULL) + 420);
-  client->prekey_profile =
-      otrng_prekey_profile_build(1234, create_privkey_v4__assign);
+static void orchestration_fixture_teardown(orchestration_fixture_s *f,
+                                           gconstpointer user_data) {
+  (void)user_data;
 
-  otrng_client_ensure_correct_state(client);
+  free(f->callbacks);
+  otrng_client_free(f->client);
+  otrng_list_free_nodes(f->gs->clients);
+  free(f->gs);
+  free(f->long_term_key);
+  free(f->forging_key);
+  otrng_client_profile_free(f->client_profile);
+  otrng_prekey_profile_free(f->prekey_profile);
+}
+
+static otrng_client_profile_s *
+create_client_profile_copy_from(const otrng_client_profile_s *src) {
+  otrng_client_profile_s *result =
+      otrng_xmalloc_z(sizeof(otrng_client_profile_s));
+  otrng_client_profile_copy(result, src);
+  return result;
+}
+
+static otrng_prekey_profile_s *
+create_prekey_profile_copy_from(const otrng_prekey_profile_s *src) {
+  otrng_prekey_profile_s *result =
+      otrng_xmalloc_z(sizeof(otrng_prekey_profile_s));
+  otrng_prekey_profile_copy(result, src);
+  return result;
+}
+
+static void test__otrng_client_ensure_correct_state__creates_new_long_term_key(
+    orchestration_fixture_s *f, gconstpointer data) {
+  (void)data;
+
+  create_privkey_v4__assign = f->long_term_key;
+  f->client->client_profile =
+      create_client_profile_copy_from(f->client_profile);
+  f->client->prekey_profile =
+      create_prekey_profile_copy_from(f->prekey_profile);
+
+  otrng_client_ensure_correct_state(f->client);
 
   g_assert_cmpint(load_privkey_v4__called, ==, 1);
   g_assert_cmpstr(load_privkey_v4__called_with.protocol, ==, "test-otr");
@@ -165,75 +224,28 @@ test__otrng_client_ensure_correct_state__creates_new_long_term_key(void) {
   g_assert_cmpstr(create_privkey_v4__called_with.account, ==, "sita@otr.im");
 
   g_assert_cmpint(store_privkey_v4__called, ==, 1);
-  g_assert(store_privkey_v4__called_with == client);
+  g_assert(store_privkey_v4__called_with == f->client);
 
-  g_assert(client->client_profile == NULL);
-  g_assert(client->prekey_profile == NULL);
+  g_assert(f->client->client_profile == NULL);
+  g_assert(f->client->prekey_profile == NULL);
 
-  store_privkey_v4__called_with = NULL;
-  create_privkey_v4__assign = NULL;
-  otrng_client_free(client);
-  temp_client = NULL;
-  otrng_list_free_nodes(gs.clients);
+  f->client->keypair = NULL;
 }
 
 static void
-test__otrng_client_ensure_correct_state__fails_creating_long_term_key(void) {
-  otrng_client_callbacks_s callbacks;
-  uint8_t sym1[ED448_PRIVATE_BYTES] = {1};
-  uint8_t sym2[ED448_PRIVATE_BYTES] = {2};
-  otrng_keypair_s forging_key, long_term;
-  otrng_client_profile_s *client_profile;
-  otrng_prekey_profile_s *prekey_profile;
-  otrng_global_state_s gs = {
-      .clients = NULL,
-      .callbacks = &callbacks,
-      .user_state_v3 = NULL,
-  };
-  otrng_client_id_s cid = {
-      .protocol = "test-otr",
-      .account = "sita@otr.im",
-  };
-  otrng_client_s *client = otrng_client_new(cid);
-  client->global_state = &gs;
-  gs.clients = otrng_list_add(client, gs.clients);
-  callbacks.load_privkey_v4 = load_privkey_v4;
-  callbacks.store_privkey_v4 = store_privkey_v4;
-  callbacks.create_privkey_v4 = create_privkey_v4;
-  callbacks.load_client_profile = load_client_profile;
-  callbacks.load_prekey_profile = load_prekey_profile;
-  callbacks.store_client_profile = store_client_profile;
-  callbacks.store_prekey_profile = store_prekey_profile;
-  callbacks.load_prekey_messages = load_prekey_messages;
-  callbacks.store_prekey_messages = store_prekey_messages;
-  callbacks.load_forging_key = load_forging_key;
-  callbacks.store_forging_key = store_forging_key;
-  callbacks.create_forging_key = create_forging_key;
-  callbacks.create_client_profile = create_client_profile;
-  callbacks.store_expired_client_profile = store_expired_client_profile;
-  callbacks.load_expired_client_profile = load_expired_client_profile;
-  callbacks.store_expired_prekey_profile = store_expired_prekey_profile;
-  callbacks.load_expired_prekey_profile = load_expired_prekey_profile;
-  callbacks.create_prekey_profile = create_prekey_profile;
+test__otrng_client_ensure_correct_state__fails_creating_long_term_key(
+    orchestration_fixture_s *f, gconstpointer data) {
+  otrng_client_profile_s *copy_cp =
+      create_client_profile_copy_from(f->client_profile);
+  otrng_prekey_profile_s *copy_pp =
+      create_prekey_profile_copy_from(f->prekey_profile);
 
-  temp_client = client;
+  (void)data;
 
-  load_privkey_v4__called = 0;
-  store_privkey_v4__called = 0;
-  create_privkey_v4__called = 0;
+  f->client->client_profile = copy_cp;
+  f->client->prekey_profile = copy_pp;
 
-  load_privkey_v4__assign = NULL;
-  create_privkey_v4__assign = NULL;
-
-  otrng_keypair_generate(&long_term, sym1);
-  otrng_keypair_generate(&forging_key, sym2);
-  client_profile = otrng_client_profile_build(
-      1234, "4", &long_term, forging_key.pub, time(NULL) + 420);
-  prekey_profile = otrng_prekey_profile_build(1234, &long_term);
-  client->client_profile = client_profile;
-  client->prekey_profile = prekey_profile;
-
-  otrng_client_ensure_correct_state(client);
+  otrng_client_ensure_correct_state(f->client);
 
   g_assert_cmpint(load_privkey_v4__called, ==, 1);
   g_assert_cmpstr(load_privkey_v4__called_with.protocol, ==, "test-otr");
@@ -245,142 +257,58 @@ test__otrng_client_ensure_correct_state__fails_creating_long_term_key(void) {
 
   g_assert_cmpint(store_privkey_v4__called, ==, 0);
 
-  g_assert(client->keypair == NULL);
+  g_assert(f->client->keypair == NULL);
 
-  g_assert(client->client_profile == client_profile);
-  g_assert(client->prekey_profile == prekey_profile);
-
-  store_privkey_v4__called_with = NULL;
-  create_privkey_v4__assign = NULL;
-  otrng_client_free(client);
-  otrng_list_free_nodes(gs.clients);
+  g_assert(f->client->client_profile == copy_cp);
+  g_assert(f->client->prekey_profile == copy_pp);
 }
 
-static void test__otrng_client_ensure_correct_state__loads_long_term_key(void) {
-  otrng_client_callbacks_s callbacks;
-  uint8_t sym1[ED448_PRIVATE_BYTES] = {1};
-  otrng_global_state_s gs = {
-      .clients = NULL,
-      .callbacks = &callbacks,
-      .user_state_v3 = NULL,
-  };
-  otrng_client_id_s cid = {
-      .protocol = "test-otr",
-      .account = "sita2@otr.im",
-  };
-  otrng_client_s *client = otrng_client_new(cid);
-  client->global_state = &gs;
-  gs.clients = otrng_list_add(client, gs.clients);
-  callbacks.load_privkey_v4 = load_privkey_v4;
-  callbacks.store_privkey_v4 = store_privkey_v4;
-  callbacks.create_privkey_v4 = create_privkey_v4;
-  callbacks.load_client_profile = load_client_profile;
-  callbacks.load_prekey_profile = load_prekey_profile;
-  callbacks.store_client_profile = store_client_profile;
-  callbacks.store_prekey_profile = store_prekey_profile;
-  callbacks.load_prekey_messages = load_prekey_messages;
-  callbacks.store_prekey_messages = store_prekey_messages;
-  callbacks.load_forging_key = load_forging_key;
-  callbacks.store_forging_key = store_forging_key;
-  callbacks.create_forging_key = create_forging_key;
-  callbacks.create_client_profile = create_client_profile;
-  callbacks.store_expired_client_profile = store_expired_client_profile;
-  callbacks.load_expired_client_profile = load_expired_client_profile;
-  callbacks.store_expired_prekey_profile = store_expired_prekey_profile;
-  callbacks.load_expired_prekey_profile = load_expired_prekey_profile;
-  callbacks.create_prekey_profile = create_prekey_profile;
+static void test__otrng_client_ensure_correct_state__loads_long_term_key(
+    orchestration_fixture_s *f, gconstpointer data) {
+  (void)data;
 
-  temp_client = client;
+  load_privkey_v4__assign = f->long_term_key;
 
-  load_privkey_v4__called = 0;
-  store_privkey_v4__called = 0;
-  create_privkey_v4__called = 0;
-
-  create_privkey_v4__assign = NULL;
-
-  load_privkey_v4__assign = otrng_keypair_new();
-  otrng_keypair_generate(load_privkey_v4__assign, sym1);
-
-  otrng_client_ensure_correct_state(client);
+  otrng_client_ensure_correct_state(f->client);
 
   g_assert_cmpint(load_privkey_v4__called, ==, 1);
   g_assert_cmpstr(load_privkey_v4__called_with.protocol, ==, "test-otr");
-  g_assert_cmpstr(load_privkey_v4__called_with.account, ==, "sita2@otr.im");
+  g_assert_cmpstr(load_privkey_v4__called_with.account, ==, "sita@otr.im");
 
   g_assert_cmpint(create_privkey_v4__called, ==, 0);
   g_assert_cmpint(store_privkey_v4__called, ==, 0);
 
-  store_privkey_v4__called_with = NULL;
-  load_privkey_v4__assign = NULL;
-  otrng_client_free(client);
-  otrng_list_free_nodes(gs.clients);
+  f->client->keypair = NULL;
 }
 
 static void
 test__otrng_client_ensure_correct_state__doesnt_load_long_term_key_if_already_exists(
-    void) {
-  otrng_client_callbacks_s callbacks;
-  uint8_t sym1[ED448_PRIVATE_BYTES] = {1};
-  otrng_global_state_s gs = {
-      .clients = NULL,
-      .callbacks = &callbacks,
-      .user_state_v3 = NULL,
-  };
-  otrng_client_id_s cid = {
-      .protocol = "test-otr",
-      .account = "sita2@otr.im",
-  };
-  otrng_client_s *client = otrng_client_new(cid);
-  client->global_state = &gs;
-  gs.clients = otrng_list_add(client, gs.clients);
-  callbacks.load_privkey_v4 = load_privkey_v4;
-  callbacks.store_privkey_v4 = store_privkey_v4;
-  callbacks.create_privkey_v4 = create_privkey_v4;
-  callbacks.load_client_profile = load_client_profile;
-  callbacks.load_prekey_profile = load_prekey_profile;
-  callbacks.store_client_profile = store_client_profile;
-  callbacks.store_prekey_profile = store_prekey_profile;
-  callbacks.load_prekey_messages = load_prekey_messages;
-  callbacks.store_prekey_messages = store_prekey_messages;
-  callbacks.load_forging_key = load_forging_key;
-  callbacks.store_forging_key = store_forging_key;
-  callbacks.create_forging_key = create_forging_key;
-  callbacks.create_client_profile = create_client_profile;
-  callbacks.store_expired_client_profile = store_expired_client_profile;
-  callbacks.load_expired_client_profile = load_expired_client_profile;
-  callbacks.store_expired_prekey_profile = store_expired_prekey_profile;
-  callbacks.load_expired_prekey_profile = load_expired_prekey_profile;
-  callbacks.create_prekey_profile = create_prekey_profile;
+    orchestration_fixture_s *f, gconstpointer data) {
+  (void)data;
+  f->client->keypair = f->long_term_key;
 
-  temp_client = client;
-
-  load_privkey_v4__called = 0;
-  store_privkey_v4__called = 0;
-  create_privkey_v4__called = 0;
-
-  client->keypair = otrng_keypair_new();
-  otrng_keypair_generate(client->keypair, sym1);
-
-  otrng_client_ensure_correct_state(client);
+  otrng_client_ensure_correct_state(f->client);
 
   g_assert_cmpint(load_privkey_v4__called, ==, 0);
   g_assert_cmpint(create_privkey_v4__called, ==, 0);
   g_assert_cmpint(store_privkey_v4__called, ==, 0);
 
-  otrng_client_free(client);
-  otrng_list_free_nodes(gs.clients);
+  f->client->keypair = NULL;
 }
 
+#define WITH_O_FIXTURE(_p, _c)                                                 \
+  WITH_FIXTURE(_p, _c, orchestration_fixture_s, orchestration_fixture)
+
 void units_orchestration_add_tests(void) {
-  g_test_add_func(
+  WITH_O_FIXTURE(
       "/orchestration/ensure_correct_state/long_term_key/creates",
       test__otrng_client_ensure_correct_state__creates_new_long_term_key);
-  g_test_add_func("/orchestration/ensure_correct_state/long_term_key/loads",
-                  test__otrng_client_ensure_correct_state__loads_long_term_key);
-  g_test_add_func(
+  WITH_O_FIXTURE("/orchestration/ensure_correct_state/long_term_key/loads",
+                 test__otrng_client_ensure_correct_state__loads_long_term_key);
+  WITH_O_FIXTURE(
       "/orchestration/ensure_correct_state/long_term_key/ensures",
       test__otrng_client_ensure_correct_state__doesnt_load_long_term_key_if_already_exists);
-  g_test_add_func(
+  WITH_O_FIXTURE(
       "/orchestration/ensure_correct_state/long_term_key/fails",
       test__otrng_client_ensure_correct_state__fails_creating_long_term_key);
 }
