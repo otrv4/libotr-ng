f0fc27db6a2c06a94078c616e95a071b09b8e614
Ola Bini <ola@olabini.se>
2019-04-05 07:59:51 -0500

    Not sure if this is better

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -16,10 +16,10 @@ echo `which clang`
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ -f $GPG_ERROR_DIR/src/.libs/libgpg-error.so ]]; then
-        (cd $GPG_ERROR_DIR && sudo -E -i '(cd $GPG_ERROR_DIR && make install)')
+        (cd $GPG_ERROR_DIR && sudo -E -i (cd $GPG_ERROR_DIR && make install))
     else
         curl https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.26.tar.bz2 | tar xjf - -C .deps
-        (cd $GPG_ERROR_DIR && ./configure && make -j && sudo -E -i '(cd $GPG_ERROR_DIR && make install)')
+        (cd $GPG_ERROR_DIR && ./configure && make -j && sudo -E -i (cd $GPG_ERROR_DIR && make install))
     fi
 
     # if [[ -f $LIBGCRYPT_DIR/src/.libs/libgcrypt.so ]]; then
