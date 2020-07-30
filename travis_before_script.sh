0e4e06aad39c4400d2d04678250afb5b75c5cec1
Sofía Celi <cherenkovd69@gmail.com>
2020-07-30 04:08:43 -0500

    Remove cache

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -28,20 +28,11 @@ MAKE_INSTALL="sudo -E -i $SOURCE_DIR/run_make_install.sh"
 echo `which clang`
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-
-    if [[ -f $GPG_ERROR_DIR/src/.libs/libgpg-error.so ]]; then
-        $MAKE_INSTALL $SOURCE_DIR/$GPG_ERROR_DIR
-    else
-        curl https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.26.tar.bz2 | tar xjf - -C .deps
+	curl https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.26.tar.bz2 | tar xjf - -C .deps
         (cd $GPG_ERROR_DIR && ./configure && make -j && $MAKE_INSTALL $SOURCE_DIR/$GPG_ERROR_DIR)
-    fi
 
-    if [[ -f $LIBSODIUM_DIR/src/libsodium/.libs/libsodium.so ]]; then
-        $MAKE_INSTALL $SOURCE_DIR/$LIBSODIUM_DIR
-    else
         curl https://download.libsodium.org/libsodium/releases/LATEST.tar.gz | tar xzf - -C .deps
         (cd $LIBSODIUM_DIR && ./autogen.sh && ./configure && make -j && $MAKE_INSTALL $SOURCE_DIR/$LIBSODIUM_DIR)
-    fi
 fi
 if [[ "$T" = "ctgrind" ]]; then
     if [[ -f $CTGRIND_DIR/memcheck/vgpreload_memcheck-amd64-linux.so ]]; then
@@ -58,19 +49,12 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     (cd $VALGRIND_DIR && ./configure && make -j && make install)
 fi
 
-# if [[ -f $LIBOTR_DIR/src/.libs/libotr.so ]]; then
-#     (cd $LIBOTR_DIR && sudo make install)
-# else
 if [[ ! -e $LIBOTR_DIR ]]; then
     git clone --depth=1 https://bugs.otr.im/lib/libotr.git $LIBOTR_DIR
 fi
 (cd $LIBOTR_DIR && ./bootstrap && ./configure && make -j && $MAKE_INSTALL $SOURCE_DIR/$LIBOTR_DIR)
 # fi
 
-if [[ -f $LIBGOLDILOCKS_DIR/src/.libs/libgoldilocks.so ]]; then
-    $MAKE_INSTALL $SOURCE_DIR/$LIBGOLDILOCKS_DIR
-else
-    rm -rf $LIBGOLDILOCKS_DIR
-    git clone --depth=1 https://bugs.otr.im/otrv4/libgoldilocks $LIBGOLDILOCKS_DIR
-    (cd $LIBGOLDILOCKS_DIR && ./autogen.sh && ./configure && make -j && $MAKE_INSTALL $SOURCE_DIR/$LIBGOLDILOCKS_DIR)
-fi
+rm -rf $LIBGOLDILOCKS_DIR
+git clone --depth=1 https://bugs.otr.im/otrv4/libgoldilocks $LIBGOLDILOCKS_DIR
+(cd $LIBGOLDILOCKS_DIR && ./autogen.sh && ./configure && make -j && $MAKE_INSTALL $SOURCE_DIR/$LIBGOLDILOCKS_DIR)
